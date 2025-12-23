eef694811ef5349951cd1d3251d84834bfae6cb9
David Nguyen <david@nguyen.eu>
2025-12-23 17:18:54 +0100

    Apply suggestions from code review

--- graphite/setup.macos.sh
+++ graphite/setup.macos.sh
@@ -5,7 +5,7 @@ brew install withgraphite/tap/graphite
 
 echo ""
 echo "Sign in at https://app.graphite.dev/activate to enable your token."
-read -p "Open the link now? [y/N] " -n 1 -r
+read -p "Open the link now? [y/N] " -n 1 -r || true
 echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     open "https://app.graphite.dev/activate"
